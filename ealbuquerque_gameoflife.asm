c0c938f3bc6e5c3a747c83f44ce79c59708fe459
Estevam Albuquerque <estevamgalvao@gmail.com>
2019-04-16 14:44:02 -0300

    func copyToDisplay on progress

--- ealbuquerque_gameoflife.asm
+++ ealbuquerque_gameoflife.asm
@@ -1,20 +1,22 @@
 .data
-	display:	.byte	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	display:	.byte	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				
+
 	
 	matriz:		.space	324 # preencho 18x18 espaços com 0
 	
@@ -55,9 +57,9 @@
 #	s2: Ponto X Atual
 #	s3: Ponto Y Atual
 #	s4: Endereço inicial matriz
-#	s5:
-#	s6:
-#	s7:
+#	s5: Endereço inicial display
+#	s6: Contador de colunas
+#	s7: Contador de linhas
 #	s8:
 #	s9:
 #	s10:
@@ -66,6 +68,9 @@
 
 
 .text 
+
+main:
+
 	la	t0, display 	#carrego o endereço do primeiro pixel do display
 	la	t2, white 	#carrego o endereço da cor WHITE
 	lw	t1, 0(t2) 	#carrego o valor de 0 casas descoladas do endereço de WHITE
@@ -82,17 +87,54 @@
 	ecall
 				#evitando usar o mov porque ele altera regis. temporários
 	la	t2, auxAdress	#carrego o endereço de auxAdress
-	sw	a0, 0(t2)		#armazeno o valor de a0 (qtd de pontos) em auxAdress
+	sw	a0, 0(t2)	#armazeno o valor de a0 (qtd de pontos) em auxAdress
 	lw	s1, 0(t2) 	#leio o valor guardado em auxAdress e salvo em s1
+	
+	
+	call 	writeUser
 	
 	#### EXIT ####
 	li	a7, 10
 	ecall
 	##############
+
+
+
+copyToDisplay:
+	la	s4, matriz	#carrego o endereço do primeiro pixel da matriz
+	addi	s4, s4, 19	#faço o endereço de início ir uma linha pra baixo e uma coluna pro lado 18x18 -> 19 casas
+	
+	la	s5, display	#carrego o endereço do primeiro pixel do display
+loopCopyToDisplay:
+
+	li	s6, s6, 15
+	li	s7, s7, 15
+	
+	lw	t0, 0(s4)	#carrego o valor do pixel atual na matriz
+	lw	t1, 0(s5)	#carrego o valor do pixel atual no display
+	
+	bne  	t0,t1,inverseDisplay
+	
+	addi	s4, s4, 1	#ando um byte inteiro no "ponteiro" da matriz
+	addi	s5, s5, 4	#ando 4 bytes pois a exibição no display é em word
+	
+	addi	s6, s6, -1
+	beqz 	s6, loop2CopyToDisplay
+	
+
+
+
+inverseDisplay:
+	not 	t2, t1
+	sw	t2, 0(s5)
+	
+loop2CopyToDisplay:
+		
 	
 	
-######################################### FUNÇÃO DE ESCRITA ##############################################
-write:
+	
+######################################### FUNÇÃO DE ESCRITA DOS PONTOS DO USUÁRIO ##############################################
+writeUser:
 	la	s4, matriz
 	addi	s4, s4, 19	#faço o endereço de início ir uma linha pra baixo e uma coluna pro lado 18x18 -> 19 casas
 	
@@ -107,7 +149,7 @@ write:
 	ecall
 				#evitando usar o mov porque ele altera regis. temporários
 	la	t2, auxAdress	#carrego o endereço de auxAdress
-	sw	a0, 0(t2)		#armazeno o valor de a0 (ponto X) em auxAdress
+	sw	a0, 0(t2)	#armazeno o valor de a0 (ponto X) em auxAdress
 	lw	s2, 0(t2) 	#leio o valor guardado em auxAdress e salvo em s2
 	
 	
@@ -122,10 +164,26 @@ write:
 	ecall
 				#evitando usar o mov porque ele altera regis. temporários
 	la	t2, auxAdress	#carrego o endereço de auxAdress
-	sw	a0, 0(t2)		#armazeno o valor de a0 (ponto Y) em auxAdress
+	sw	a0, 0(t2)	#armazeno o valor de a0 (ponto Y) em auxAdress
 	lw	s3, 0(t2) 	#leio o valor guardado em auxAdress e salvo em s3
 		
 	
+	# preciso deslocar meu registrador de endereço para o ponto inserido pelo usuário
+	# isto é, a distância R do início da matriz até o ponto do usuário => R = 18*Y + X
+	
+	slli	t0,s3,4		#shifto o valor de s3 (Y) 4 vezes -> Y*16 e salvo em t0
+	slli	s3,s3,1		#shifto o valor de s3 (Y) 1 vez -> Y*2 e salvo em s3
+	add	s3,s3,t0	#somo o valor de t0 com o de s3 -> Y*16 + Y*2 => Y*18
+	
+	add	s4, s4, s3	
+	add	s4, s4, s2
+	
+	not	t0, 0(s4)	#inverto o valor de todos os bits do endereço e em seguida salvo no mesmo endereço
+	sw	t0, 0(s4)	#setei a matriz para começar sempre com 0, então meus valores serão sempre -1 ou 0 dentro da matriz
+	
+	ret
+
+
 	
 	
 	
