da262b2993b9fa89ee8189f57509ca7c9feef8be
Estevam Albuquerque <estevamgalvao@gmail.com>
2019-04-19 22:40:20 -0300

    thanks god

--- getPoints.asm
+++ getPoints.asm
@@ -34,6 +34,23 @@
 				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	
+	auxMatrix:	.word	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+				0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 	stringStart:	.asciz 	"Digite o numero de pontos a serem inseridos: "
 	stringX:	.asciz	"Digite o ponto X: "
@@ -50,7 +67,7 @@
 	#	s2: Ponto Y atual
 	#	s3: Endereço Matriz
 	#	s4: Endereço Display
-	#	s5: Constante de delay 10000
+	#	s5: Endereço Matriz Auxiliar
 	#	s6: 
 	#	s7: 
 	#	s8:
@@ -65,6 +82,7 @@ getPoints:	#Conseguir X e Y, os quais o usuário gostaria de pintar no display e
 
 	la	s3, matrix
 	la	s4, display
+	la	s5, auxMatrix
 	
 	li	a7, 4
 	la	a0, stringStart	#printo na tela a String de início
@@ -114,8 +132,21 @@ loopPoints:
 	
 	
 	addi 	sp, sp, -12
-	sw	s4, 8(sp)
+	sw	s5, 8(sp)
 	sw 	s3, 4(sp)
+	sw 	ra, 0(sp)
+	
+	call	updateAuxMatrix
+	
+	lw 	ra, 0(sp)
+	addi 	sp, sp, 12
+
+
+
+
+	addi 	sp, sp, -12
+	sw	s4, 8(sp)
+	sw 	s5, 4(sp)
 	sw 	ra, 0(sp)
 	
 	call	updateDisplay
@@ -123,36 +154,39 @@ loopPoints:
 	lw 	ra, 0(sp)
 	addi 	sp, sp, 12
 
+
 	#######################
 	# ATUALIZEI O DISPLAY #
 	#######################
 	
-	li	s5, 100000
+	li	s6, 10000
 	
 firstDelay:
 
-	addi	s5, s5, -1
-	bnez 	s5, firstDelay
+	addi	s6, s6, -1
+	bnez 	s6, firstDelay
 	
 loopKeepPlaying:
 
-	addi	sp, sp, -8
+	addi	sp, sp, -12
+	sw	s5, 8(sp)
 	sw	s3, 4(sp)
 	sw	ra, 0(sp)
 	
 	call	play
 	
 	lw 	ra, 0(sp)
-	addi 	sp, sp, 8
+	addi 	sp, sp, 12
 	
 	#######################
 	# JOGUEI GAME OF LIFE #
 	#######################
 	
 	
+	
 	addi 	sp, sp, -12
 	sw	s4, 8(sp)
-	sw 	s3, 4(sp)
+	sw 	s5, 4(sp)
 	sw 	ra, 0(sp)
 	
 	call	updateDisplay
@@ -164,12 +198,28 @@ loopKeepPlaying:
 	# ATUALIZEI O DISPLAY #
 	#######################
 	
-	li	s5, 100000
+	
+	addi 	sp, sp, -12
+	sw	s5, 8(sp)
+	sw 	s3, 4(sp)
+	sw 	ra, 0(sp)
+	
+	call	updateMatrix
+	
+	lw 	ra, 0(sp)
+	addi 	sp, sp, 12
+	
+	############################################
+	# ATUALIZEI A MATRIZ PARA A PRÓXIMA RODADA #
+	############################################
+	
+	
+	li	s6, 10000
 	
 delay:
 
-	addi	s5, s5, -1
-	bnez 	s5, delay
+	addi	s6, s6, -1
+	bnez 	s6, delay
 	
 	j	loopKeepPlaying
 	
@@ -224,8 +274,7 @@ userStampMatrix: ##################### MARCAR NA MATRIZ O PIXEL ESCOLHIDO PELO U
 
 updateDisplay: ##################### ATUALIZAR O DISPLAY DE ACORDO COM A MATRIZ ####################
 
-	lw	t0, 4(sp)	#salvando o endereço inicial da matriz em T0
-	addi	t0, t0, 76	#saltando 19(x4) casa para ir de fato na malha últil da matriz (pular bordas iniciais)
+	lw	t0, 4(sp)	#salvando o endereço inicial da matriz auxiliar em T0
 	
 	lw	t1, 8(sp)	#salvando o endereço do display em T1
 	
@@ -234,19 +283,19 @@ updateDisplay: ##################### ATUALIZAR O DISPLAY DE ACORDO COM A MATRIZ 
 	
 compareDisplay:
 
-	lw	t4, 0(t0)	#carrego o valor do pixel na matriz e salvo em T4
-	lw	t5, 0(t1)	#carrego o valor do pixel no display e salvo em T5
+	lw	t4, 0(t0)	#carrego o valor do pixel da matriz auxiliar e salvo em T4
+	lw	t5, 0(t1)	#carrego o valor do pixel do display e salvo em T5
 	
 	bne   	t4, t5, paintDisplay	#comparo se eles são iguais, caso não, vou para label de pintar o display
 
 updateDisplayContinue:
 
-	beqz  	t3, jumpBorder	#caso meu contador de colunas chegou ao fim, significa que eu tenho que pular bordas na matriz
+	beqz  	t3, jumpLine	#caso meu contador de colunas chegou ao fim, significa que eu tenho que decrementar o contador de linhas
 	
 	
 jumpPixel:
 	
-	addi	t0, t0, 4	#incremento meu ponteiro da matriz para próxima casa
+	addi	t0, t0, 4	#incremento meu ponteiro da matriz auxiliar para próxima casa
 	addi	t1, t1, 4	#incremento meu ponteiro do display para próxima casa
 	addi	t3, t3, -1	#decremento meu contador de colunas
 	j	compareDisplay
@@ -257,11 +306,11 @@ paintDisplay:
 	sw	t5, 0(t1)	#pinto ele no endereço apontado pelo "ponteiro" do display
 	j	updateDisplayContinue	#volto a função de updateDisplay para verificar os outros pixels
 
-jumpBorder:
+jumpLine:
 	
 	beqz 	t6, returnFromDisplay	#caso eu entrei aqui, ou seja, minhas colunas já são 0 e meu T6 também for0, significa que minha matriz acabou
 	
-	addi	t0, t0, 12	#pulo 3 casas para chegar no próximo pixel da linha de baixo pulando as bordas
+	addi	t0, t0, 4	#incremento meu ponteiro da matriz auxiliar para próxima casa
 	addi	t1, t1, 4	#incremento meu ponteiro do display para próxima casa
 	addi	t6, t6, -1	#decremento meu contador de linhas pois desci uma linha
 	
@@ -279,15 +328,140 @@ returnFromDisplay:
 
 
 
-play: ##################### GAME OF LIFE ATUALIZANDO A MATRIZ ####################
+
+updateAuxMatrix: ##################### ATUALIZAR O MATRIZ AUXILIAR DE ACORDO COM A MATRIZ ####################
 
 	lw	t0, 4(sp)	#salvando o endereço inicial da matriz em T0
 	addi	t0, t0, 76	#saltando 19(x4) casa para ir de fato na malha últil da matriz (pular bordas iniciais)
 	
+	lw	t1, 8(sp)	#salvando o endereço do AuxMatrix em T1
+	
+	li	t3, 15		#colunas
+	li	t6, 15		#linhas
+	
+compareAuxMatrix:
+
+	lw	t4, 0(t0)	#carrego o valor do pixel da matriz e salvo em T4
+	lw	t5, 0(t1)	#carrego o valor do pixel do AuxMatrix e salvo em T5
+	
+	bne   	t4, t5, paintAuxMatrix	#comparo se eles são iguais, caso não, vou para label de pintar o AuxMatrix
+
+updateAuxMatrixContinue:
+
+	beqz  	t3, jumpAuxMatrixBorder	#caso meu contador de colunas chegou ao fim, significa que eu tenho que pular bordas na matriz
+	
+	
+jumpAuxMatrixPixel:
+	
+	addi	t0, t0, 4	#incremento meu ponteiro da matriz para próxima casa
+	addi	t1, t1, 4	#incremento meu ponteiro do AuxMatrix para próxima casa
+	addi	t3, t3, -1	#decremento meu contador de colunas
+	j	compareAuxMatrix
+
+paintAuxMatrix:
+
+	not	t5, t5		#inverto o valor do pixel no AuxMatrix
+	sw	t5, 0(t1)	#pinto ele no endereço apontado pelo "ponteiro" do AuxMatrix
+	j	updateAuxMatrixContinue	#volto a função de updateAuxMatrix para verificar os outros pixels
+
+jumpAuxMatrixBorder:
+	
+	beqz 	t6, returnFromAuxMatrix	#caso eu entrei aqui, ou seja, minhas colunas já são 0 e meu T6 também for0, significa que minha matriz acabou
+	
+	addi	t0, t0, 12	#pulo 3 casas para chegar no próximo pixel da linha de baixo pulando as bordas
+	addi	t1, t1, 4	#incremento meu ponteiro da AuxMatrix para próxima casa
+	addi	t6, t6, -1	#decremento meu contador de linhas pois desci uma linha
+	
+	li	t3, 15		#reinicio meu contador de colunas
+
+	j	compareAuxMatrix	#voltar para comparar o pŕoximo pixel
+
+returnFromAuxMatrix:
+	
+	ret			#retornar a main
+	
+
+
+
+
+updateMatrix: ##################### ATUALIZAR O MATRIZ DE ACORDO COM A MATRIZ AUXILIAR ####################
+
+	lw	t0, 4(sp)	#salvando o endereço inicial da matriz em T0
+	addi	t0, t0, 76	#saltando 19(x4) casa para ir de fato na malha últil da matriz (pular bordas iniciais)
+	
+	lw	t1, 8(sp)	#salvando o endereço da matriz auxiliar em T1
+	
+	li	t3, 15		#colunas
+	li	t6, 15		#linhas
+	
+compareMatrix:
+
+	lw	t4, 0(t0)	#carrego o valor do pixel da matriz e salvo em T4
+	lw	t5, 0(t1)	#carrego o valor do pixel do Matrix e salvo em T5
+	
+	bne   	t4, t5, paintMatrix	#comparo se eles são iguais, caso não, vou para label de pintar o Matrix
+
+updateMatrixContinue:
+
+	beqz  	t3, jumpMatrixBorder	#caso meu contador de colunas chegou ao fim, significa que eu tenho que pular bordas na matriz
+	
+	
+jumpMatrixPixel:
+	
+	addi	t0, t0, 4	#incremento meu ponteiro da matriz para próxima casa
+	addi	t1, t1, 4	#incremento meu ponteiro da matriz auxiliar para próxima casa
+	addi	t3, t3, -1	#decremento meu contador de colunas
+	j	compareMatrix
+
+paintMatrix:
+
+	not	t4, t4		#inverto o valor do pixel na matriz
+	sw	t4, 0(t0)	#pinto ele no endereço apontado pelo "ponteiro" da matriz
+	j	updateMatrixContinue	#volto a função de updateMatrix para verificar os outros pixels
+
+jumpMatrixBorder:
+	
+	beqz 	t6, returnFromMatrix	#caso eu entrei aqui, ou seja, minhas colunas já são 0 e meu T6 também for0, significa que minha matriz acabou
+	
+	addi	t0, t0, 12	#pulo 3 casas para chegar no próximo pixel da linha de baixo pulando as bordas
+	addi	t1, t1, 4	#incremento meu ponteiro da matriz auxiliar para próxima casa
+	addi	t6, t6, -1	#decremento meu contador de linhas pois desci uma linha
+	
+	li	t3, 15		#reinicio meu contador de colunas
+
+	j	compareMatrix	#voltar para comparar o pŕoximo pixel
+
+returnFromMatrix:
+	
+	ret			#retornar a main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+play: ##################### GAME OF LIFE ATUALIZANDO A MATRIZ ####################
+	
+	
+	lw	t6, 8(sp)	#salvando o endereço inicial da matriz auxiliar em T6
+	lw	t0, 4(sp)	#salvando o endereço inicial da matriz em T0
+	addi	t0, t0, 76	#saltando 19(x4) casa para ir de fato na malha últil da matriz (pular bordas iniciais)
+	
+	
+	
 	li	t1, 15		#colunas
 	li	t2, 15		#linhas
 	li	t5, -2
-	li	t6, -3
+
 	
 sumNeighborhood:
 	
@@ -322,21 +496,23 @@ sumNeighborhood:
 	beqz 	t4, deadPixel
 	
 alivePixel:
-	
-	beq 	t3, t5, nextPixel	
-	beq 	t3, t6, nextPixel
+	li	t5, -2
+	beq 	t3, t5, nextPixel
+	li	t5, -3	
+	beq 	t3, t5, nextPixel
 	j	switchLife
 
 deadPixel:
 	
-	beq 	t3, t6, switchLife
+	li	t5, -3	
+	beq 	t3, t5, switchLife
 	j	nextPixel
 	
 
 switchLife:
 	
 	not	t4, t4
-	sw	t4, 0(t0)
+	sw	t4, 0(t6)
 	j	nextPixel
 	
 nextPixel:
@@ -347,6 +523,7 @@ nextPixel:
 jumpPlayPixel:
 	
 	addi	t0, t0, 4	#incremento meu ponteiro da matriz para próxima casa
+	addi	t6, t6, 4	#incremento meu ponteiro da matriz auxiliar para próxima casa
 	addi	t1, t1, -1	#decremento meu contador de colunas
 	j	sumNeighborhood
 	
@@ -356,6 +533,7 @@ jumpPlayBorder:
 	beqz 	t2, returnFromPlay	#caso eu entrei aqui, ou seja, minhas colunas já são 0 e meu T2 também for0, significa que minha matriz acabou
 	
 	addi	t0, t0, 12	#pulo 3 casas para chegar no próximo pixel da linha de baixo pulando as bordas
+	addi	t6, t6, 4	#incremento meu ponteiro da matriz auxiliar para próxima casa
 	addi	t2, t2, -1	#decremento meu contador de linhas pois desci uma linha
 	
 	li	t1, 15		#reinicio meu contador de colunas
@@ -366,19 +544,3 @@ jumpPlayBorder:
 returnFromPlay:
 	
 	ret
-
-	
-	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
